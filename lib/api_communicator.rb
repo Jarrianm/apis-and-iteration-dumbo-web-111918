41f4b8c7dd355821fd1b12f7505c6b939932ece5
Jarrian McLean <jarrian.mclean@gmail.com>
2018-11-20 16:47:56 +0000

    automatically backed up by learn

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -2,11 +2,13 @@ require 'rest-client'
 require 'json'
 require 'pry'
 
-def get_character_movies_from_api(character_name)
+def get_character_movies_from_api ¸(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
+  character_list - response_hash["results"]
 
+binding.pry
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
   # collect those film API urls, make a web request to each URL to get the info
@@ -19,7 +21,10 @@ def get_character_movies_from_api(character_name)
 end
 
 def print_movies(films)
-  # some iteration magic and puts out the movies in a nice list
+list = films.each do |film|
+response_string = RestClient.get(film)
+response_hash = JSON.parse(response_string)
+puts response_hash["title"]
 end
 
 def show_character_movies(character)
